0682a762e0ff3246208d80ffd84b114ccc3efca1
Eduard-Konovka <ed098ua@gmail.com>
2021-11-04 22:13:51 +0200

    Adds task 1, step 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Component } from 'react';
+// import PropTypes from 'prop-types';
+// import shortid from 'shortid';
+import Container from './components/Container';
+import Section from './components/Section';
+import ReviewsWidget from './components/ReviewsWidget';
+import PhoneBook from './components/PhoneBook';
 import './App.css';
 
-export default function App() {
-  return 'Hello world!';
+export default class App extends Component {
+  render() {
+    return (
+      <Container>
+        <Section title="1. Виджет отзывов">
+          <ReviewsWidget />
+        </Section>
+
+        <Section title="2. Телефонная книга">
+          <PhoneBook />
+        </Section>
+      </Container>
+    );
+  }
 }
